e711f4a3de8290ae2df1882ad217472202ec1ca3
iboyeon0816 <iboyeon0816@gmail.com>
2024-02-01 00:22:27 +0900

    [FEAT] 프로젝트 모집 글 작성 API 200 -> 201 응답으로 수정

--- src/main/java/com/sphere/demo/web/controller/ProjectRestController.java
+++ src/main/java/com/sphere/demo/web/controller/ProjectRestController.java
@@ -1,6 +1,7 @@
 package com.sphere.demo.web.controller;
 
 import com.sphere.demo.apipayload.ApiResponse;
+import com.sphere.demo.apipayload.status.SuccessStatus;
 import com.sphere.demo.converter.project.ProjectConverter;
 import com.sphere.demo.domain.Project;
 import com.sphere.demo.service.project.ProjectCommandService;
@@ -8,11 +9,9 @@ import com.sphere.demo.web.dto.ProjectRequestDto.CreateDto;
 import com.sphere.demo.web.dto.ProjectResponseDto.CreateResultDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/projects")
@@ -21,10 +20,11 @@ public class ProjectRestController {
 
     private final ProjectCommandService projectCommandService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ApiResponse<CreateResultDto> createProject(@AuthenticationPrincipal Long userId,
                                                       @RequestBody @Valid CreateDto createDto) {
         Project project = projectCommandService.createProject(userId, createDto);
-        return ApiResponse.onSuccess(ProjectConverter.toCreateResultDto(project));
+        return ApiResponse.of(SuccessStatus._CREATED, ProjectConverter.toCreateResultDto(project));
     }
 }
